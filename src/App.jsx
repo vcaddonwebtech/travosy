29a58417b292925067cdf30dabe91133ba98b393
vcaddonwebtech <vcaddonwebtech@gmail.com>
2026-02-27 19:01:27 +0530

    navbar incomplte

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Tagline from './Components/layout/Tagline'
+import Navbar from './Components/layout/Navbar'
 function App() {
   return (
     <>
+    
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Tagline />} />
         </Routes>
       </BrowserRouter>
-
-
+      <Navbar/>
+      
     </>
   )
 }
