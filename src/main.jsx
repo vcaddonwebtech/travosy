6e484dfc3fd723df3380e6e6a7f5251b09172640
vcaddonwebtech <vcaddonwebtech@gmail.com>
2026-02-27 17:48:20 +0530

    Tagline infobar is Completed

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import {store} from "./Components/store/index.js"
 import {Provider} from 'react-redux'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <Provider store = {store}>
     <App />
 
     </Provider>
-  </StrictMode>
+  
 )
